b9eaa54cadd3e36e7d3c2c4af6f7350adb74b9cf
abdulhameed81 <139330747+abdulhameed81@users.noreply.github.com>
2024-01-12 00:40:03 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "I Love My Self";
+        return "Mubeen Is My Emotion";
     }
 
 }
